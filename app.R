3e22fb7320e3a8505b0ac5789c8f734347466614
Ally Krinsky <amk8500@uw.edu>
2019-03-09 12:40:29 -0800

    adding stuff

---
+++ app.R
@@ -0,0 +1,12 @@
+library("shiny")
+
+my_ui <- fluidPage({
+  
+})
+
+my_server <- function(input, output){
+  
+}
+
+
+shinyApp(ui = my_ui, server = my_server)
---
+++ my_ui.R
@@ -0,0 +1,5 @@
+library("shiny")
+
+my_ui <- fluidPage({
+  
+})
