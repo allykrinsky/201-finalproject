c414c27d0f7e7766db8916e87d8e51204767c01e
Carter Drew <dev@uw.edu>
2019-03-09 14:37:55 -0800

    create visualization function for carters question

--- app.R
+++ app.R
@@ -1,11 +1,17 @@
 library("shiny")
+source("carter.R")
 
-my_ui <- fluidPage({
-  
-})
+
+my_ui <- fluidPage(
+  radioButtons("ag_type", "Aggregation Type:",
+               list("Average Closing" = 1,
+                 "Maximum Closing" = 2,
+                 "Minimum Closing" = 3), 1),
+  plotOutput("dow_monthly")
+)
 
 my_server <- function(input, output){
-  
+  output$dow_monthly <- renderPlot({create_monthly_dow_graph(input$ag_type)})
 }
 
 
--- carter.R
+++ carter.R
@@ -1,4 +1,22 @@
-dow <- read.csv("data/DOW")
-gdp <- read.csv("data/GDP")
+library(ggplot2)
 
+dow <- read.csv("data/DOW.csv")
+gdp <- read.csv("data/GDP.csv")
 
+# How does the time of year affect the Dow Jones?
+create_monthly_dow_graph <- function(ag_type){
+  if(is.null(ag_type)){
+    ag_type <- "1"
+  }
+  
+  ag_types <- list("1" = "average_close", "2" = "max_close", "3" = "min_close")
+  
+  dow_monthly <- dow %>% 
+    mutate(month = substr(label, 1, 4), year = substr(date, 1, 4), month_num = substr(date, 6, 7)) %>% 
+    group_by(month, year, month_num) %>% 
+    summarise(average_close = mean(close), max_close = max(close), min_close = min(close), average_volume = mean(volume)) %>% 
+    select(month_num, month, year, value = ag_types[[ag_type]])
+  
+  ggplot(data = dow_monthly) +
+    geom_line(mapping = aes(x = month_num, y = value, group = year, color = year))
+}
