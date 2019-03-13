90e97c445b4126d00e6f94b04152c4de738dd99e
Ally Krinsky <amk8500@uw.edu>
2019-03-12 17:50:05 -0700

    pre merge

--- intro_page.R
+++ intro_page.R
@@ -9,23 +9,16 @@ largely influence the overall economy of the US. Another one is the GDP for Unit
 quarterly GDP to measure the US economy. GDP refers to gross domestic product, which measures the total value of goods and 
 services produced in a given amount of time. In the GDP data table, we have GDP for each individual state for each quarter 
 in all different industries including construction, mannufactuer and et. Using these two data sets, we are going to have a 
-better understanding of the US economy, and their interrelation. For more background knowledge on economics to help 
-understand our report"
+better understanding of the US economy, and their interrelation. "
 
-tab_1 <- "Under the GDP vs. Dow tab, you will find information answering the question, 'How does change in the Dow Jones Index 
+tab_1 <- HTML(paste0("Under the ","<b>","GDP vs. Dow tab, ","</b>", "you will find information answering the question, 'How does change in the Dow Jones Index 
 affect the change in US GDP?'. These two fields are measures of the US econmoy and from our preliminary analysis seem to 
-follow a similiar pattern. To learn more on the relationship between the Dow Jones Index and US GDP, click on the GDP vs. Dow tab."
+follow a similiar pattern. To learn more on the relationship between the Dow Jones Index and US GDP, click on the GDP vs. Dow tab."))
 
 
 tab_intro <- "Our report has three tabs: Dow Jones Seasonal, GDP VS. Dow Jones, and United States GDP. These three tabs will help three questions respectively."
 
-# three tab description will inlcude the question, significance of the question, how the user can navigate the tab. 
-
-
-
-
-
-third_tab <- paste("United States GDP Tab will provide help answer the question: Is there any correlation between geographic locations and GDP?",
+third_tab <- HTML(paste0("The ","<b>","United States GDP Tab","</b>"," will provide help  to answer the question: Is there any correlation between geographic locations and GDP?",
                    "The question will provide the user  with a better understanding of the US economy by providing the GDP stats in individual states and region. 
                    The user can choose two regions including states and larger areas, and a certain industry. Then the graph will show the U.S. GDP trend and GDP 
                    tend in these two regions from 2005 to 2018 in each quarter in the certain industry. If the user choose to compare two states’ GDP change, the 
@@ -35,7 +28,7 @@ third_tab <- paste("United States GDP Tab will provide help answer the question:
                    always shown on the graph because it provides the user with the overview of the country’s GDP change. The table will provide the user a quantitative 
                    analysis on the GDP changes for these regions from 2005 to 2018. Combining the graph and table, the user can understand US economy from different perspectives.",
                    sep = "\n"
-)
+))
                    
                    
                    
